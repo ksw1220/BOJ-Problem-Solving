2d8c234d4397f7288a235bf49f5bc091e1cf4f34
Soonwoo Kwon <louis981220@gmail.com>
2021-04-24 15:33:13 +0900

    Solve 5525

---
+++ 5525.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+void count(int start, int len, string S, vector<int> &v){
+    int cnt=0;
+    for(int i=start;i<len-2;i+=2){
+        if(S[i]=='I'&&S[i+1]=='O'&&S[i+2]=='I'){
+            cnt++;
+        }
+        else {
+            if(cnt!=0)
+                v.push_back(cnt);
+            cnt=0;
+        }
+    }
+     if(cnt!=0)
+        v.push_back(cnt);
+}
+int main(){
+    int N,len;
+    string S;
+    cin>>N>>len;
+    cin>>S;
+    vector<int> fragment;
+    int answer=0;
+    count(0, len, S, fragment);
+    count(1, len, S, fragment);
+    for(int i=0;i<fragment.size();i++){
+        int tmp=fragment[i];
+        if(tmp>=N){
+            answer+=tmp-N+1;
+        }
+    }
+
+    printf("%d",answer);
+    return 0;
+}
