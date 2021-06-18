a8628b3cfc283b048d7149de45d752a7677c1054
권순우 <soonwookwon@gwonsun-uui-MacBookPro.local>
2021-06-19 08:14:34 +0900

    기타줄

---
+++ 1049.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, M;
+    int package = 1001, individual = 1001;
+    int a, b;
+    int answer = 0;
+    scanf("%d %d", &N, &M);
+    while (M--)
+    {
+        scanf("%d %d", &a, &b);
+        if (a < package)
+            package = a;
+        if (b < individual)
+            individual = b;
+    }
+
+    if (package >= individual * 6)
+    {
+        answer = individual * N;
+    }
+    else
+    {
+        if (N % 6 * individual > package)
+        {
+            answer = N / 6 * package + package;
+        }
+        else
+            answer = N / 6 * package + N % 6 * individual;
+    }
+    printf("%d\n", answer);
+    return 0;
+}
