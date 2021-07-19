7fa5c43d31c9fd25496dd8588c29934381b24cee
Soonwoo Kwon <louis981220@gmail.com>
2021-07-19 12:02:50 +0900

    N과M(9)

---
+++ 15663.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> numbers;
+vector<bool> visited;
+vector<vector<int>> isExist;
+int N, M;
+
+bool isEqual(const vector<int> &A, const vector<int> &B)
+{
+    if (A.size() != B.size())
+        return false;
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] != B[i])
+            return false;
+    }
+    return true;
+}
+void Combination(int idx, vector<int> &answer)
+{
+    visited[idx] = true;
+    answer.push_back(numbers[idx]);
+    if (answer.size() == M)
+    {
+        bool chk = false;
+        for (int i = 0; i < isExist.size(); i++)
+        {
+            if (isEqual(isExist[i], answer))
+            {
+                chk = true;
+                break;
+            }
+        }
+        if (!chk)
+        {
+            isExist.push_back(answer);
+            for (int i = 0; i < M; i++)
+            {
+                printf("%d ", answer[i]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (!visited[i])
+                Combination(i, answer);
+        }
+    }
+    visited[idx] = false;
+    answer.pop_back();
+}
+int main()
+{
+    vector<int> tmp;
+    scanf("%d %d", &N, &M);
+
+    numbers.assign(N, 0);
+    visited.assign(N, false);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+
+    sort(numbers.begin(), numbers.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        if (i == 0 || numbers[i - 1] != numbers[i])
+        {
+            Combination(i, tmp);
+        }
+    }
+    return 0;
+}
