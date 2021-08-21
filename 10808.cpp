835dbbe6d6aad219b67f155380049967c9bafc74
Soonwoo Kwon <louis981220@gmail.com>
2021-08-21 10:35:12 +0900

    알파벳 개수

---
+++ 10808.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int alpha[26];
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        alpha[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%d ",alpha[i]);
+    }
+    return 0;
+}
