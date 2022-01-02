7d3fa5fbc0b5f458be77e46a746a43ca8575a909
Soonwoo Kwon <louis981220@gmail.com>
2022-01-02 23:47:09 +0900

    1학년

--- 5557.cpp
+++ 5557.cpp
@@ -4,82 +4,48 @@
 using namespace std;
 
 int N;
-vector<int> numbers;
-vector<vector<int>> dp;
+vector<long> numbers;
+vector<vector<long long>> dp;
 
-int makeExp(int equality)
+long long makeExp()
 {
-    int re = 0;
+    long long re = 0;
     dp[0][numbers[0]] = 1;
-    for (int i = 1; i < equality; i++)
+    for (int i = 1; i < N - 1; i++)
     {
         int val = numbers[i];
         for (int j = 0; j <= 20; j++)
         {
-            if (j + val <= 20)
+            if (dp[i - 1][j])
             {
-                printf("a");
-                dp[i][j + val] = dp[i - 1][j];
-            }
-            if (j - val >= 0)
-            {
-                dp[i][j - val] = dp[i - 1][j];
-            }
-        }
-    }
-    dp[equality][numbers[equality]] = 1;
-    for (int i = equality + 1; i < N; i++)
-    {
-        int val = numbers[i];
-        for (int j = 0; j <= 20; j++)
-        {
-            if (j + val <= 20)
-            {
-                dp[i][j + val] = dp[i - 1][j];
-            }
-            if (j - val >= 0)
-            {
-                dp[i][j - val] = dp[i - 1][j];
+                if (j + val <= 20)
+                {
+                    dp[i][j + val] += dp[i - 1][j];
+                }
+                if (j - val >= 0)
+                {
+                    dp[i][j - val] += dp[i - 1][j];
+                }
             }
         }
     }
 
-    for (int i = 0; i <= 20; i++)
-    {
-        re += dp[equality - 1][i] * dp[N - 1][i];
-    }
+    re = dp[N - 2][numbers[N - 1]];
 
-    cout << "equl " << equality << endl;
-    for (int i = 0; i <= 20; i++)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j <= 20; j++)
-        {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
     return re;
 }
 int main()
 {
-    int answer = 0;
+    long long answer = 0;
     cin >> N;
     numbers.assign(N, 0);
+    dp.assign(N, vector<long long>(21, 0));
     for (int i = 0; i < N; i++)
     {
         cin >> numbers[i];
     }
-    for (int i = 1; i < N; i++)
-    {
-        dp.assign(N, vector<int>(21, 0));
-        answer += makeExp(i);
-    }
+
+    answer = makeExp();
 
     cout << answer << endl;
     return 0;
