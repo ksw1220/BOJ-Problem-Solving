d20a6e8ec9cb9fce6cae5284a4c37cdf8b88f877
Soonwoo Kwon <louis981220@gmail.com>
2022-01-13 17:30:47 +0900

    앱

---
+++ 7579.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX_COST 100000
+using namespace std;
+
+vector<int> cost, memory;
+vector<vector<int>> dp;
+int main()
+{
+    int N, M;
+
+    cin >> N >> M;
+    cost.resize(N + 1);
+    memory.resize(N + 1);
+    dp.assign(N + 1, vector<int>(MAX_COST + 1, 0));
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> memory[i];
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> cost[i];
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j <= MAX_COST; j++)
+        {
+            if (j >= cost[i])
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + memory[i]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+
+    for (int i = 0; i <= MAX_COST; i++)
+    {
+        int tmp = 0;
+        for (int j = 1; j <= N; j++)
+        {
+            tmp = max(tmp, dp[j][i]);
+        }
+        if (tmp >= M)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
